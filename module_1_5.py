78ea79a16f10b4aaabc46936f166cc04fde2bebe
Tori-Fly <paiette511@gmail.com>
2024-10-31 11:33:45 +0300

    Add files via upload
    Изменяемые и неизменяемые объекты. Кортежи

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = (1, False, "lemon", 5.5)
+print(immutable_var)
+# immutable_var[0] = "Осень" кортеж - неизменяемая коллекция элементов
+print(immutable_var)
+mutable_list = [20, 'Dream', 'Happy']
+print(mutable_list)
+mutable_list[1] = 'Eternity'
+print(mutable_list)
